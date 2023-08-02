f839a1f720aa3f41aff64a95189f2921735487ce
BullChallenger <am23am45@naver.com>
2023-08-02 11:29:37 +0900

    [FEAT] WebSocketConfig 의 registerWebSocketHandlers Method 완성

--- src/main/java/com/knockin/door/knockin_onheavens_door/config/WebSocketConfig.java
+++ src/main/java/com/knockin/door/knockin_onheavens_door/config/WebSocketConfig.java
@@ -1,5 +1,8 @@
 package com.knockin.door.knockin_onheavens_door.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.knockin.door.knockin_onheavens_door.webSocket.SocketTextHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -7,10 +10,15 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-
+        registry.addHandler(new SocketTextHandler(objectMapper), "/user")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
